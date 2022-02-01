f814f61f14cb85be880198a90494a13afcb12b69
Kento-Ishizaki <kentoron1115@gmail.com>
2022-02-02 08:05:22 +0900

    Feat: FirstPostページ作成

---
+++ next-app/pages/posts/first-post.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+const FirstPost: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <h1>First Post</h1>
+    </div>
+  );
+};
+
+export default FirstPost;
