33f13a602e7782d24e47f26bc15f7b525ffe84b4
Kento-Ishizaki <kentoron1115@gmail.com>
2022-02-02 08:02:06 +0900

    Refac: コンポーネントを変数に格納

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -39,16 +40,6 @@ export default function Home() {
             <h2>Examples &rarr;</h2>
             <p>Didddscover and deploy boilerplate example Next.js projects.</p>
           </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
@@ -66,4 +57,6 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
+
+export default Home;
